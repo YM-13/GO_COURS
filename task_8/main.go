aa17bc696b00067eca7c95be242f52cff60173ed
YM-13 <exitospec1.com>
2024-04-15 19:48:06 -0700

    t8 2

--- task_8/main.go
+++ task_8/main.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"fmt"
 	"strconv"
@@ -15,22 +16,40 @@ var num int
 var flag bool = true
 var everige int
 
-//СЛАЙС МЭЙКЕР 1НАЙТИ СРЕДНЕЕ АРИФМЕТИЧЕСКОЕ 2 ОТСОРТИРОВАТЬ ЭЛЕМЕНТЫ
-func slice_sorted () {}
 
-func main () {
+// СЛАЙС МЭЙКЕР 1НАЙТИ СРЕДНЕЕ АРИФМЕТИЧЕСКОЕ 2 ОТСОРТИРОВАТЬ ЭЛЕМЕНТЫ
+func slice_sorted(e_n int, args ... int) {
+	for _, n := range args {
+		if n > e_n {
+			res = append(res, n)
+		}
+	}
+}
+
+func min_num (args ... int) int {
+	var min_n int = args[0]
+	for _, n := range args[1:] {
+		if n < min_n {
+			min_n = n
+		}
+	}
+	return min_n
+}
+func main() {
+	pln("Enter the number. To complete entering numbers, enter 's'")
 	for flag {
-		pln("Enter the number. To complete entering numbers, enter 'stop'")
 		fmt.Scan(&s)
 
-
-		if s == "stop" {
+		if s == "s" {
 			flag = false
+			everige = sum_nums / len(numbers)
 			//НАПЕЧАТАТЬСЛАЙС С ОТСОРТИРОВАННЫМИ ЧИСЛАМИ
+			slice_sorted(everige, numbers...)
 			pln(res)
 
 			//НАПЕЧАТАТЬ МИНИМВАЛЬНЫЙ ЭЛЕМЕНТ СЛАЙСА
-			pln("Min num of slice: ")
+			//pln(min_num(numbers...))
+			pln("Min num of slice: ", min_num(numbers...))
 			break
 		}
 
@@ -42,7 +61,6 @@ func main () {
 
 		numbers = append(numbers, num)
 		sum_nums += num
-		pln(sum_nums)
 	}
 
 }

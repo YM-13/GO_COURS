7633350916544b0f68963fb4cb31b021feba907f
YM-13 <exitospec1.com>
2024-05-14 19:37:51 -0700

    66-4 last optimizing

--- GO_LeetCode/66.Plus_One/main.go
+++ GO_LeetCode/66.Plus_One/main.go
@@ -3,48 +3,28 @@ package main
 import "fmt"
 
 func plusOne(digits []int) []int {
-    var last_index int
-    var last_new_dig int
-	var res [] int
-	var new_d int
-	var time_var int = 0
-
-    last_index = len(digits) - 1
-    last_new_dig = digits[last_index] + 1
-
-    if last_new_dig < 10 {
-        digits[last_index] = last_new_dig
-        return digits
-
-    } else {
-		time_var = 1
-		// новый слайс длина старого + 1 = [0, 0, 0, ...]. Для случая с 10 в новом слайсе "0" уже есть
-		res = make([]int, len(digits) + 1)
-
-        for i := last_index - 1; i > -1; i-- {
-			if time_var == 1 {
-				new_d = digits[i] + 1
-				if new_d < 10 {
-					time_var = 0
-					res[i + 1] = new_d
-					res1 := digits[: i]
-					res2 := res[i + 1 :]
-					res = append(res1, res2...)
-					return res
-				}else { // new_d == 10
-					res[i + 1] = 0
-				}
-
-
-        	}
-
-    	}
-		res[0] = 1
-    return res
+	dg := digits
+	var l_i int = len(dg) - 1
+	if dg[l_i] < 9 {
+		dg[l_i] = dg[l_i] + 1
+		return dg
 	}
+	// новый вариант 14.05.24
+	for i := l_i; i > -1; i-- {
+		if dg[i] == 9 {
+			dg[i] = 0
+		} else {
+			dg[i] = dg[i] + 1
+			return dg
+		}
+	}
+	res := []int {1}
+	res = append(res, dg...)
+
+	return res
 }
 
 func main() {
-	var a = [] int {8}
+	var a = [] int {8, 9, 9}
 	fmt.Println(plusOne(a))
 }

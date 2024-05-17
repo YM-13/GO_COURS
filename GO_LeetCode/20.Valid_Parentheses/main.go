84a6e9513a30675bc114086a6368f93e761caf27
YM-13 <exitospec1.com>
2024-05-16 20:48:44 -0700

    20_1

--- GO_LeetCode/20.Valid_Parentheses/main.go
+++ GO_LeetCode/20.Valid_Parentheses/main.go
@@ -1,8 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// ПОПАСТЬСЯ ДОЛЖЕН ПРАВИЛЬНЫЙ СИМВОЛ
+
 
 func isValid(s string) bool {
-	
+	var s_st string = "({["
+	var s_end string = "]})"
 
+	ss := strings.Split(s, "")
+	fmt.Println(ss)
+	if len(s) % 2 != 0 {
+		fmt.Println("---")
+		return false
+	}else if strings.Contains(s_st, ss[0]) != true || strings.Contains(s_end, ss[len(s) - 1]) != true {
+		fmt.Println("---")
+		return false
+	}
+	fmt.Println("+++")
+	return true
+}
+
+func main() {
+	isValid("[({{}[]()}}[]]{}")
 }

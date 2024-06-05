2fe7e357cf68b02267b7c7d27f7fe3e59bd32008
YM-13 <exitospec1.com>
2024-06-05 14:24:01 -0700

    20_6

--- GO_LeetCode/20.Valid_Parentheses/main.go
+++ GO_LeetCode/20.Valid_Parentheses/main.go
@@ -13,54 +13,49 @@ import (
 )
 
 func isValid(s string) bool {
-    if len(s) % 2 != 0 {
+    if len(s) % 2 != 0 || len(s) < 2 {
 		return false
     }
+
 	rs := []rune(s)
 	var m_v = map[rune]rune {40: 41, 123: 125, 91: 93,}
-	flag := 0
+	//var prev int
 	count := 0
 
-	for i, r := range(rs) {
-        flag = 0
-		if r == 0 {
+	for i := 0; i < len(rs) - 1; i++ {
+		if rs[i] == 0 {
 			continue
-		}else if _, ok := m_v[r]; ok == false {
-			return false
 		}
-
-
+		//prev = i
 		for ii := i + 1; ii < len(rs); ii += 2 {
-			if rs[ii] == r {
-				count += 1
-			}else if rs[ii] == 0 {
-				return false
-			}else if rs[ii] == m_v[r] {
-				if count == 0 {
-					fmt.Println(count)
-					rs[ii] = 0
-                	flag = 1
-					break
-				} else {
-					fmt.Println("tut0000")
-					count -= 1
-					flag = 1
+			if rs[ii] == 0 {
+				continue
+			}else if m_v[rs[i]] == rs[ii] {// rs[i] - key from map
+				// ПРОВЕРИТЬ ПРОХОДОМ В ОБРАТНОМ ПОРЯДКЕ НЕТ ЛИ ОТКРІВАЮЩЕЙ СКОБКИ
+				// ЕСЛИ ЕСТЬ, ТО "0" ПРИСВОИТЬ ЕЙ
+				for b := ii - 1; -1 < b; b -= 2 {
+					if rs[b] == rs[i] {
+						rs[b] = 0
+						rs[ii] = 0
+						count += 2
+						break
+					}
 				}
 			}
 		}
-		//fmt.Println(count)
-		fmt.Println(rs)
-
 	}
-	if flag == 0 {
-		fmt.Println("tut")
-        return false
-    }
-	return true
+	fmt.Println(rs)
+	if count == len(rs) {
+		return true
+	}else {
+		return false
+	}
+
+
 }
 
 func main() {
 	// fmt.Println(isValid("(}]]{}"))
 	// fmt.Println(isValid("[{}]"))
-	fmt.Println(isValid("(([]){})")) // true
+	fmt.Println(isValid("[({])}")) // false
 }
